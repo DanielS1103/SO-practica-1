f0a1107d5b463eca1c8d98958c263f76c5a63d7b
Daniel Acosta <danielacosta130602@gmail.com>
2025-07-13 21:10:52 -0500

    factorial

--- parcial II/factoral_threads.c
+++ parcial II/factoral_threads.c
@@ -20,7 +20,7 @@ void* calculate_partial(void* arg){
     data->partial_result = 1;
 
     for(int i = data -> start; i <= data -> end; i++){
-        data -> partial_result = *= i;
+        data -> partial_result *= i;
     }
 
     sem_wait(&semaphore);
@@ -34,8 +34,25 @@ int main(){
     pthread_t threads[NUM_THREADS];
     ThreadData thread_data[NUM_THREADS];
 
-    sem_init(&semaphote, 0, 1);
+    sem_init(&semaphore, 0, 1);
 
     int segment = TARGET / NUM_THREADS;
     int start = 1;
+
+    for(int i = 0; i < NUM_THREADS; i++){
+        thread_data[i].start = start;
+        thread_data[i].end = (i == NUM_THREADS - 1) ? TARGET : start + segment - 1;
+        start = thread_data[i].end + 1;
+
+        pthread_create(&threads[i], NULL, calculate_partial, &thread_data[i]);
+    }
+
+    for(int i = 0; i < NUM_THREADS; i++){
+        pthread_join(threads[i], NULL);
+    }
+
+    sem_destroy(&semaphore);
+    printf("El factorial de %d es: %llu\n", TARGET, global_result);
+
+    return 0;
 }
